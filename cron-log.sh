314805d2a19bdec66f4cc4bb9648a795492145c7
ojooss <53396131+ojooss@users.noreply.github.com>
2021-02-19 19:11:10 +0100

    call script via bash

--- cron-log.sh
+++ cron-log.sh
@@ -8,4 +8,4 @@
 #  best way to import is calling this script at 23:59:00 to read total result of current day
 #
 
-docker-compose run --rm app /app/batch.sh "/app/logs/speedtest.$(date +%F).log"
+docker-compose run --rm app /bin/bash /app/batch.sh "/app/logs/speedtest.$(date +%F).log"
